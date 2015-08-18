336215659ea161e4da3a868bf804aebaca1a3ebb
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-18 22:15:43 +0000

    Add basic Grid and Vector constructors/prototypes.

---
+++ main_test.spec.js
@@ -0,0 +1,24 @@
+var Grid = require('./main').Grid;
+var Vector = require('./main').Vector;
+
+
+describe("Grid", function () {
+  var grid;
+
+  beforeEach(function() {
+    grid = new Grid(5, 5);
+  });
+
+  afterEach(function() {
+    grid = null;
+  });
+
+  it("cannot get a nonexistent vector", function () {
+    expect(grid.get(new Vector(1, 1))).toBe(undefined);
+  });
+
+  it("can get a vector", function () {
+    grid.set(new Vector(1, 1), "X");
+		expect(grid.get(new Vector(1, 1))).toBe("X");
+  });
+});
